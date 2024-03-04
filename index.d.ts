8ea9c1d1339cc8c5e59b62199184eaa73b7b26f3
JonnyJong <jonnyjong@outlook.com>
2024-03-04 13:43:31 +0800

    🎉 init: v1

--- index.d.ts
+++ index.d.ts
@@ -1,459 +1,237 @@
-// Type definitions for Ezal 0.0.10
+// Type definitions for Ezal 1.0.0
 // Project: https://github.com/JonnyJong/ezal
 // Definitions by: Jonny <https://github.com/JonnyJong>
 // Definitions: https://github.com/JonnyJong/ezal
 
-declare module 'ezal'{
-  /**
-   * Add a listener on ezal event
-   * @param type Event type
-   * @param listener Event listener
+import { NormalToc } from "ezal-markdown";
+
+type Async<T> = T | Promise<T>;
+type SerializedData = string | number | boolean | null | SerializedData[] | { [key: string]: SerializedData };
+
+declare module 'ezal' {
+  /* 
+    Global
+    全局
    */
-  export function addListener(type: 'init', listener: ()=>any): void
-  export function addListener(type: 'init-pages', listener: ()=>any): void
-  export function addListener(type: 'pre-render', listener: (page: Page | Post)=>any): void
-  export function addListener(type: 'post-render', listener: (page: Page | Post, variables: any)=>any): void
-  export function addListener(type: 'pre-generate', listener: (page: Page | Post)=>any): void
-  export function addListener(type: 'post-generate', listener: (generated: {page: Page | Post, html: string})=>any): void
-  export function addListener(type: 'pre-style', listener: (styleContent: {stylus: string, css: ''})=>any): void
-  export function addListener(type: 'post-style', listener: (styleContent: {stylus: string, css: string})=>any): void
-  export function addListener(type: 'pre-assets', listener: ()=>any): void
-  export function addListener(type: 'post-assets', listener: ()=>any): void
   /**
-   * Set pug option in HTML template
+   * Ezal 选项
    */
-  export let pug: {
-    [x: string | number | symbol]: any,
-    }
-  /**
-   * Set stylus option in CSS template
-   */
-  export let stylus: {
+  export interface IEzalConfig<ThemeConfig> {
     /**
-     * Set variables that can be obtained in stylus by get('keyName.keyName')
+     * 源路径
+     * 默认为`src`
      */
-    var: {
-      [x: string | number | symbol]: any,
+    source?: string,
+    /**
+     * 输出路径
+     * 默认为`dist`
+     */
+    out?: string,
+    /**
+     * 主题名称
+     * 将按照以下优先级寻找主题文件
+     * - ./themes/<name>
+     * - ./themes/ezal-theme-<name>
+     * - ./node_modules/ezal-theme-<name>
+     */
+    themeName: string,
+    /**
+     * 插件
+     */
+    plugins: IPlugin[],
+    /**
+     * 站点选项
+     */
+    site: {
+      /**
+       * 站点标题
+       */
+      title: string,
+      /**
+       * 站点副标题
+       */
+      subtitle?: string,
+      /**
+       * 站点描述
+       */
+      description?: string,
+      /**
+       * 站点关键词
+       */
+      keywords?: string[],
+      /**
+       * 作者
+       */
+      author: string,
+      /**
+       * 站点语言
+       */
+      language: string,
+      /**
+       * 站点域名
+       */
+      host: string,
+      /**
+       * 站点根目录
+       * 默认`/`
+       */
+      root?: string,
+      /**
+       * 时区
+       * 文章使用时间不为 UTC 时间时建议使用
+       * 默认为 UTC+00:00
+       */
+      timezone?: number,
     },
     /**
-     * Set the functions that can be called in stylus
+     * 主题选项
      */
-    function: {
-      [x: string | number | symbol]: Function,
-    }
-  }
-  export namespace render {
-    type MarkdownRenderResult = {
-      context: string,
-      variables: any,
-    }
+    theme?: ThemeConfig,
     /**
-     * Render Markdown content to HTML
-     * @param source Markdown content
-     * @returns HTML string
+     * 编译选项
      */
-    function markdown(source: string, v?: MarkdownExtensionVariables, paragraph?: boolean): Promise<MarkdownRenderResult>
-    function markdownLine(source: string, v?: MarkdownExtensionVariables): Promise<MarkdownRenderResult>
-    /**
-     * Render HTML template to HTML
-     * @param layoutName Layout's name in theme's layout folder
-     * @param options Set pug option in HTML template
-     * @returns HTML string
-     */
-    function pug(layoutName: 'post' | 'page' | string, options: any): Promise<string>
-    /**
-     * Render stylus to CSS
-     * @param stylusContext Stylus content
-     * @param options Set stylus option in CSS template
-     * @returns CSS string
-     */
-    function stylus(stylusContext: string, options: any): Promise<string>
-    /**
-     * Render and wrap codeblock
-     * Modify this function to customize the codeblock
-     */
-    let codeblock: (matched: MarkdownMatched, v: MarkdownExtensionVariables)=>string
+    compile?: {
+      /**
+       * 直接将 Markdown 文件渲染成 HTML 文件
+       * 默认`false`
+       * - false: `src/page/example.md`->`dist/page/example/index.html`
+       * - true: `src/page/example.md`->`dist/example.html`
+       */
+      trailingDotHTML?: boolean,
+      /**
+       * 页面链接尾随`index.html`
+       * 默认`false`
+       */
+      trailingIndexDotHTML?: boolean,
+    },
   }
   /**
-   * Ezal's config
-   * @readonly
+   * 获取所有选项
+   * @description
+   * - 获取后可以修改选项
+   * - init 阶段开始后可用
    */
-  export namespace config {
-    /**
-     * Current theme's folder name in /themes
-     * @readonly
-     */
-    const theme: string
-    const title: string
-    const subtitle: string
-    const description: string
-    const keywords: string[]
-    const author: string
-    const language: string
-    const host: string
-    const root: string
-    const timezone: number
-    const markdown: {
-      heading_anchor_prefix: string,
-      highlight_prefix: string,
-      footnote_classname: string,
-      task_list_classname: string,
-    }
+  export function getConfig(): IEzalConfig<any>;
+  /**
+   * 读取源文件
+   * @param path 源文件路径
+   * @param encoding 编码
+   */
+  export function readSrc(path: string, encoding?: BufferEncoding): Promise<string | Buffer>;
+  /**
+   * 保存为分发文件
+   * @param path 分发文件路径
+   * @param data 分发文件数据
+   * @param encoding 编码
+   */
+  export function writeDist(path: string, data: Buffer | string, encoding?: BufferEncoding): Promise<void>;
+  /**
+   * Markdown 渲染器相关
+   */
+  export * as markdown from "ezal-markdown";
+  export class IPage {
+    layout: string;
+    title?: string;
+    description?: string;
+    date: Date;
+    updated: Date;
+    permalink?: string;
+    published: boolean;
+    source: string;
+    content: string;
+    toc: NormalToc;
+    meta: { [name: string]: SerializedData };
   }
-  /**
-   * Theme's config
-   */
-  export let theme: {
-    [x: string | number | symbol]: any,
+  export class IPost extends IPage {
+    layout: 'post';
+    tags: Set<ITag>;
+    categories: Set<ICategory>;
+    draft: boolean;
   }
-  /**
-   * Page object model
-   */
-  export class Page{
-    /**
-     * @param path Url of page
-     * @param source Markdown source
-     */
-    constructor(path: string, source: string)
-    /**
-     * Page's origin path
-     */
-    path: string
-    /**
-     * Page's url
-     */
-    url: string
-    /**
-     * Page's title
-     */
-    title: string | null
-    /**
-     * Page's date of creation
-     */
-    date: Date
-    /**
-     * Page's date of updated
-     */
-    updated: Date
-    /**
-     * Page's HTML template
-     */
-    layout: 'page' | 'post' | string
-    /**
-     * Page's origin Markdown content
-     */
-    source: string
-    /**
-     * Page's HTML content render from source
-     */
-    context: string
-    /**
-     * Update page by markdown content
-     * @param source Markdown source
-     */
-    update(source: string): void
-    /**
-     * Remove page
-     */
-    remove(): void
+  export class ITag {
+    name: string;
+    posts: Set<IPost>;
   }
-  /**
-   * Post object model
-   */
-  export class Post extends Page{
-    /**
-     * Post's categories
-     */
-    categories: Map<Array<string>, Category>
-    /**
-     * Post's tags
-     */
-    tags: Set<Tag>
-    /**
-     * Add a tag in this post
-     * @param name Tag's name
-     */
-    addTag(name: string): void
-    /**
-     * Remove a tag in this post
-     * @param name Tag's name
-     */
-    removeTag(name: string): void
-    /**
-     * Add this post in a category
-     * @param name Tag's name
-     */
-    addCategory(name: string): void
-    removeCategory(name: string): void
-    /**
-     * Remove this post in a category
-     * @param name Tag's name
-     */
+  export class ICategory {
+    name: string;
+    posts: Set<IPost>;
+    parent: ICategory | null;
+    children: { [name: string]: ICategory };
+    getPath(): string[];
   }
-  /**
-   * All the pages
+  export function getAllTags(): Set<ITag>;
+  export function getAllRootCategories(): Set<ICategory>;
+
+  /* 
+    Plugins
+    插件
    */
-  export let pages: Set<Page>
   /**
-   * All the posts
+   * 插件
    */
-  export let posts: Set<Post>
-  /**
-   * Category array path
-   */
-  type CategoryPath = Array<string>;
-  /**
-   * Categories map
-   */
-  type Categories = Map<string, Category>;
-  /**
-   * Category root object model
-   */
-  class CategoryRoot{
+  export class IPlugin {
     /**
-     * Children category
-     */
-    children: Categories;
-    /**
-     * Find subcategories under the current category
-     * @param path Category array path
-     */
-    query(path: CategoryPath): Category | undefined;
-    /**
-     * Add subcategories under the current category
-     * @param category Category child
-     */
-    addChild(category: Category): void;
-    /**
-     * Get subcategories in the current category, or automatically create subcategories if there are none
-     * @param name Category's name
-     */
-    getChildAuto(name: string): Category;
-    /**
-     * Get category's root
-     */
-    getAll(): Array<Category>;
-    /**
-     * Get subcategories at the category root, or automatically create subcategories if there are none
-     * @param path Category array path
-     */
-    static getByPathAuto(path: CategoryPath): Category;
-    /**
-     * Add post to category by path
-     * @param path Category array path
-     * @param post Post object
-     */
-    static addPostByPath(path: CategoryPath, post: Post): Category;
-    /**
-     * Initialize categories for post
-     * @param post Post object
-     * @param object YAML parsed object
-     */
-    static initCategories(post: Post, object: any): void
-  }
-  /**
-   * Category object model
-   */
-  class Category extends CategoryRoot{
-    /**
-     * Category's name
+     * 名称
+     * @example 'ezal-plugin-xxx'
+     * @pattern ^(?:(?:@(?:[a-z0-9-*~][a-z0-9-*._~]*)?/[a-z0-9-._~])|[a-z0-9-~])[a-z0-9-._~]*$
+     * @description 命名规则同 node 包名
      */
     name: string;
     /**
-     * Posts under the category
+     * init 事件
      */
-    posts: Set<Post>;
-    /**
-     * Parent category
-     */
-    parent: Category | CategoryRoot;
-    /**
-     * Category's path
-     */
-    path: CategoryPath;
-    /**
-     * @param path Category's path
-     */
-    constructor(path: CategoryPath)
-    /**
-     * Remove category
-     */
-    remove(): void;
-    /**
-     * Add post to this category
-     * @param post Post object
-     */
-    addPost(post: Post): void;
-    /**
-     * Remove post from this category
-     * @param post Post object
-     */
-    removePost(post: Post): void;
+    onInit?(): Async<any>;
   }
   /**
-   * All the categories
+   * 源文件处理器
    */
-  export let categories: CategoryRoot;
-  /**
-   * Tag root object model
-   */
-  class Tags{
+  export class ISourceHandler<T = any> {
     /**
-     * Create a tag that cannot have the same name as an existing tag
-     * @param name Tag's name
+     * 源文件类型
+     * @example '.md'
+     * @description 类型相同时，将会覆盖已注册的处理器
      */
-    add(name: string): Tag;
+    type: string;
     /**
-     * Remove a tag
-     * @param tag Tag's name
+     * 源文件添加事件
+     * @param path 源文件路径
      */
-    delete(tag: string): boolean;
+    onAdd(path: string): Async<T>;
     /**
-     * Executes a provided function once per each tag in the tags object, in insertion order.
+     * 编译源事件
+     * @param source 预处理源
      */
-    forEach(callbackfn: (value: Tag, value2: Tag, set: Set<Tag>) => void, thisArg?: any): void;
+    onCompile(source: T): Async<any>;
     /**
-     * Check if tag exists
-     * @param name Tag's name
+     * 移除源事件
+     * @param source 预处理源
      */
-    has(name: string): boolean;
-    /**
-     * Get tag
-     * @param name Tag's name
-     */
-    get(name: string): Tag | null;
-    /**
-     * Get the tag, if it does not exist, it will be created automatically
-     * @param name Tag's name
-     */
-    autoGet(name: string): Tag;
-    /**
-     * @returns The number of tags
-     */
-    readonly size: number;
+    onRemove?(source: T): Async<any>;
   }
   /**
-   * Tag object model
+   * 注册源文件处理器
+   * @param sourceHandlers 源文件处理器
+   * @description 仅 init 阶段可用
    */
-  class Tag{
-    /**
-     * Tag's name
-     */
-    name: string;
-    /**
-     * Tag's posts
-     */
-    posts: Set<Post>;
-    /**
-     * Add post in this tag
-     * @param post Post object
-     */
-    addPost(post: Post): void;
-    /**
-     * Remove post in this tag
-     * @param post Post object
-     */
-    removePost(post: Post): void;
-    /**
-     * Remove this tag
-     */
-    remove(): void;
-  }
+  export function registerSourceHandlers(...sourceHandlers: ISourceHandler[]): void;
+  
+  /*
+    Layout
+   */
   /**
-   * All the tags
+   * Layout 编译器
    */
-  export let tags: Tags | Set<Tag>;
-  type MarkdownExtensionVariables = {
-    page?: Page | Post,
+  export type LayoutHandlers = { [name: string]: Function };
+  export class ILayoutCompiler {
     /**
-     * Shared variables when rendering markdown
+     * 编译 layout
      */
-    markdown: any,
+    compile(): Async<string>;
   }
-  export type MarkdownExtension = {
-    name: string,
-    level: 'block' | 'inline',
-    /**
-     * Match start index
-     * @param src Source string
-     * @param v Page and Markdown variables
-     */
-    start(src: string, v?: MarkdownExtensionVariables): number | null | undefined | void | Promise<number | null | undefined | void>,
-    /**
-     * Confirm and match the syntax-compliant strings
-     * @param src Source string
-     * @param v Page and Markdown variables
-     */
-    match(src: string, v?: MarkdownExtensionVariables): MarkdownMatched | null | undefined | void | Promise<MarkdownMatched | null | undefined | void>,
-    /**
-     * Render matched result to HTML string
-     * @param matched Matched result
-     * @param v Page and Markdown variables
-     */
-    render(matched: MarkdownMatched, v?: MarkdownExtensionVariables): string | Promise<string>,
-    priority?: number,
-  }
-  export type MarkdownMatched = {
-    /**
-     * Matched raw string
-     */
-    raw: string,
-    text?: string,
-    args?: string[],
-    arg?: string,
-    [x: string | number | symbol]: any,
-  }
-  export type MarkdownTag = {
-    name: string,
-    level: 'block' | 'inline',
-    end: void | undefined | boolean,
-    render(matched: MarkdownMatched, v?: MarkdownExtensionVariables): string | Promise<string>,
-    priority?: number,
-  }
-  export function setMarkdownExtension(markdownExtension: MarkdownExtension | MarkdownExtension[]): void
-  export function setMarkdownTag(markdownTag: MarkdownTag | MarkdownTag[]): void
-  type DateTemplate = {
-    D: [string, string, string, string, string, string, string],
-    DD: [string, string, string, string, string, string, string],
-    MMM: [string, string, string, string, string, string, string, string, string, string, string, string],
-    MMMM: [string, string, string, string, string, string, string, string, string, string, string, string],
-    AP: [string, string],
-    ap: [string, string],
-  }
-  export namespace util{
-    function HTMLEncode(src: string): string
-    function url_for(src: string): string
-    function full_url_for(src: string): string
-    function now(): Date
-    function parseDate(date: any, format: string | undefined | void, option: void | undefined | DateTemplate): string
-    function setDateTemplate(option: DateTemplate): void
-    function writeFile(url: string, data: any, option?: BufferEncoding): Promise<void>
-  }
-  /**
-   * Auto load locale from theme's locales folder
-   */
-  export const locale: any
-  type ProceduralItem = {
-    path: string,
-    data: any,
-  }
-  type Procedural = {
-    type: 'page' | 'assets',
-    /**
-     * When type is page, a layout must be set
-     */
-    layout?: 'page' | 'post' | string,
-    /**
-     * When type is assets, a dataType must be set
-     */
-    dataType?: BufferEncoding,
-    /**
-     * Match pages requested in serve mode
-     * @param url Require url
-     * @returns Return ProceduralItem must make the path and url the same, otherwise it is considered not matched
-     */
-    match(url: string): ProceduralItem | undefined | void | Promise<ProceduralItem | undefined | void>,
-    getItems(): ProceduralItem | ProceduralItem[] | Promise<ProceduralItem | ProceduralItem[]>,
-  }
-  export function setProceduralGenerater(arg: Procedural | Procedural[]): void
+  
+  /* 
+    Theme
+    主题
+  */
+  export function registerLayoutHandlers(layoutHandlers: LayoutHandlers): void;
 }
