eca519a2fff85a8eae72de29a6604048cc6dff19
Jonny <jonnyjong@outlook.com>
2024-04-01 19:46:09 +0800

    ✨ feat: class Tag

---
+++ src/tag.ts
@@ -0,0 +1,34 @@
+import { ITag, IPost } from "./types";
+
+let tags: Map<string, Tag> = new Map();
+
+class Tag implements ITag {
+  #name: string;
+  #posts: Set<IPost> = new Set();
+  constructor(name: string) {
+    if (typeof name !== 'string') throw new TypeError('Tag names can only be set using the string type');
+    if (tags.has(name)) throw new Error('Tag with the same name already exist.');
+    this.#name = name;
+    tags.set(name, this);
+  };
+  get name(): string {
+    return this.#name;
+  };
+  get posts(): Set<IPost> {
+    return this.#posts;
+  };
+}
+
+export function setTag(name: string, post: IPost) {
+  let tag = tags.get(name);
+  if (!tag) {
+    tag = new Tag(name);
+    tags.set(name, tag);
+  }
+  tag.posts.add(post);
+  post.tags.add(tag);
+}
+
+export function getAllTags(): ITag[] {
+  return [...tags.values()];
+}
