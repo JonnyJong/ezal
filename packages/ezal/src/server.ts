f7f4e89dc9f84057e0d3446bef20cdbd9cd19a00
Jonny <jonnyjong@outlook.com>
2026-02-18 16:02:58 +0800

    🐞 fix(ezal): Fix automatic refresh failure when setting `site.root`

--- packages/ezal/src/server.ts
+++ packages/ezal/src/server.ts
@@ -258,6 +258,8 @@ function broadcast(message: any) {
 
 export function broadcastUpdate(url: string) {
 	logger.debug(INFO.UPDATE, url);
+	const root = getConfig().site.root;
+	if (root) url = URL.join(root, url);
 	broadcast({ type: 'update', url });
 }
 
